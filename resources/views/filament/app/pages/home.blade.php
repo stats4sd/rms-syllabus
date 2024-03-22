936925f87230fdae868db456178d4d86cccb4015
Ciara McHugh <ciara@stats4sd.org>
2024-03-22 17:39:46 +0000

    adds about text

--- resources/views/filament/app/pages/home.blade.php
+++ resources/views/filament/app/pages/home.blade.php
@@ -1,5 +1,33 @@
 <x-filament-panels::page>
 
-Your journey to build research methods capacity and knowledge
+    Your journey to build research methods capacity and knowledge
+
+    <div>What is the RM Syllabus?</div>
+
+    <div>Competency Framework</div>
+    <div>
+        We've set out what we believe to be the essential research methods skills that are needed 
+        to conduct high-quality research in agroecology.
+    </div>
+
+    <div>Learning Pathway</div>
+    <div>
+        We provide a clear pathway towards developing these skills. You can create a free account 
+        to keep track of your progress.
+    </div>
+
+    <div> Modules</div>
+    <div>    
+        The pathway is structured in to modules which you can follow like an online course. The 
+        topics and module aims are mapped to the essential skills we identified.
+    </div>
+
+    <div> High Quality learning materials</div>
+    <div>
+        Develop your skills and learn about research methods using a range of materials including 
+        videos, books and interactive e-learning. Activities in the syllabus modules are hand-selected
+        from our extensive resources database.
+    </div>
+
 
 </x-filament-panels::page>
