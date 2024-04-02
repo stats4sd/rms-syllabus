6012e95a2cc4d67299bea24501bd2aafb3c9d53c
Ciara McHugh <ciara@stats4sd.org>
2024-04-02 11:24:23 +0100

    updates theme and header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,11 +1,7 @@
-<header class="top-0 pt-5 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-    <span class="text-md text-gray-500 sm:text-center dark:text-gray-400">
-        <a href="/home">RM SYLLABUS</a>
-    </span>
+<header class="top-0 pt-5 left-0 z-20 w-full p-4 bg-darkblue border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+        <h5 class="text-white"><a href="/home">RM SYLLABUS</a></h5>
     @guest
-    <span class="text-md text-gray-500 sm:text-center dark:text-gray-400 ml-auto">
-        <a href="/login">Login</a>
-    </span>
+        <h5 class="text-white ml-auto"><a href="/login">Login</a></h5>
     @endguest
     @auth
     <div class="ml-auto">
