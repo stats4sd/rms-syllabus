6a7e15d9d7775683fd69e4561e03e9c11ee044ec
Ciara McHugh <ciara@stats4sd.org>
2024-04-23 13:04:35 +0100

    adds language dropdown to header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,14 +4,41 @@
         <h5 class="text-white"><a href="/home">RM SYLLABUS</a></h5>
     @endunless
 
-    @guest
-        <h5 class="text-white ml-auto"><a href="/login">Login</a></h5>
-    @endguest
+    <div class="flex items-center ml-auto">
+        <div class="dropdown mr-4" x-data="{open: false}">
+            <h5 class="text-white"> 
+                <a
+                    class="dropdown-toggle"
+                    role="button"
+                    aria-expanded="false"
+                    x-on:click="open = !open"
+                >
+                    Change Language
+                </a>
+            </h5>
 
-    @auth
-    <div class="ml-auto">
-        <x-filament-panels::user-menu />
+            <div
+                class="dropdown-menu"
+                x-show="open"
+                x-transition.origin.top.left
+                x-on:click.outside="open = false"
+                style="display:none"
+            >
+                <a class="dropdown-item text-white" href="{{ URL::current() . '?locale=en' }}">English</a>
+                <a class="dropdown-item text-white" href="{{ URL::current() . '?locale=es' }}">Español</a>
+                <a class="dropdown-item text-white" href="{{ URL::current() . '?locale=fr' }}">Français</a>
+            </div>
+            </div>
+        </div>
+
+        @guest
+            <h5 class="text-white mr-4"><a href="/login">Login</a></h5>
+        @endguest
+
+        @auth
+            <x-filament-panels::user-menu />
+        @endauth
+
     </div>
-    @endauth
-    
+
 </header>
