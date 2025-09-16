0afb03bc1813ca4cb30f4d30bd5e03cc553b1c78
Ciara <ciara@stats4sd.org>
2025-09-16 16:24:12 +0100

    adds bullet pt option

--- app/Filament/Admin/Resources/ModuleResource.php
+++ app/Filament/Admin/Resources/ModuleResource.php
@@ -212,19 +212,19 @@ class ModuleResource extends Resource
                         ->columns(3)
                         ->schema([
                             Forms\Components\TextInput::make('guidance')->hiddenOn(['edit', 'create']),
-                            Forms\Components\Textarea::make('guidance_en')
+                            Forms\Components\RichEditor::make('guidance_en')
                                             ->label('English')
-                                            ->rows(8),
+                                            ->toolbarButtons(['bulletList']),
                                             // ->requiredWithoutAll('guidance_es, guidance_fr')
                                             // ->validationMessages(['required_without_all' => 'Enter guidance in at least one language']),
-                            Forms\Components\Textarea::make('guidance_es')
+                            Forms\Components\RichEditor::make('guidance_es')
                                             ->label('Spanish')
-                                            ->rows(8),
+                                            ->toolbarButtons(['bulletList']),
                                             // ->requiredWithoutAll('guidance_en, guidance_fr')
                                             // ->validationMessages(['required_without_all' => 'Enter guidance in at least one language']),
-                            Forms\Components\Textarea::make('guidance_fr')
+                            Forms\Components\RichEditor::make('guidance_fr')
                                             ->label('French')
-                                            ->rows(8),
+                                            ->toolbarButtons(['bulletList']),
                                             // ->requiredWithoutAll('guidance_es, guidance_en')
                                             // ->validationMessages(['required_without_all' => 'Enter guidance in at least one language']),
                         ]),
